66b251101170adeb3e8cc0521767b743ea139772
NikFMatten <nfmatten@gmail.com>
2023-05-22 10:53:24 -0600

    created auth serializers, urls and views + updated project urls

--- backend/authentication/serializer.py
+++ backend/authentication/serializer.py
@@ -4,20 +4,20 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all)])
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
 
-    def create(self, validate_data):
+    def create(self, validated_data):
         user = User.objects.create(
-            username = validate_data['username'],
-            email = validate_data['email'],
-            first_name = validate_data['first_name'],
-            last_name = validate_data['last_name']
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
-        user.set_password(validate_data['password'])
+        user.set_password(validated_data['password'])
         user.save()
         return user
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.model import User
+from django.contrib.auth.models import User
 from .serializer import RegistrationSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
